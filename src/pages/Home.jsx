1d181dcfd16928a5b5fe3dc7acea4021f9a3aa8b
ShawTheHuman <nick.braverdesign@gmail.com>
2025-11-30 13:06:55 -0600

    Add reviews section, update port to 3000

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Timeline from '../components/Timeline'
+import Reviews from '../components/Reviews'
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Timeline />
+            <Reviews />
         </>
     )
 }
